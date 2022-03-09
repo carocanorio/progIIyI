badadea142feb223111ad568a8ccdf2f985114f5
carocanorio <ccanoriohellwing@udesa.edu.ar>
2022-03-09 20:28:28 -0300

    hice gran parte de la p2 y la 3

--- programacion2/Clase1/practica2/app.js
+++ programacion2/Clase1/practica2/app.js
@@ -53,4 +53,123 @@ if (persona.edad < 18 && persona.ciudad == null){
     mensaje = 'Winter is coming'
 };
 
-console.log(mensaje);
+//console.log(mensaje);
+
+/*BUCLE FOR*/
+
+let base = 2;
+
+let resultados = [];
+
+for (let i = 0; i < 10; i++) {
+    resultados.push(base*i) 
+}
+
+//console.log(resultados);
+
+let destinos = ['Grecia', 'Londres', 'Miami', 'Orlando', 'Nueva York'];
+
+for (let i = 0; i<destinos.length; i++) {
+    //console.log('Mi destino' + [i] + 'es' + destinos[i]);
+};
+
+let notas = [10,20,30,65,70,80,90];
+
+for (let i = 0; i<notas.length; i++) {
+    //console.log('Nota:' + notas[i]);
+
+    if (notas[i]>=90) {
+        //console.log('Nota alfabética A');
+    }else if (notas[i]>=80) {
+        //console.log('Nota alfabética B');
+    }else if (notas[i]>=70) {
+        //console.log('Nota alfabética C');
+    }else if (notas[i]>=65) {
+        //console.log('Nota alfabética D');
+    }else{
+        //console.log('Nota alfabética F');
+    }
+};
+
+let ganancias = [
+    {
+        mes: 1,
+        saldo: 2
+    },
+    {
+        mes: 2,
+        saldo: -3
+    },
+    {
+        mes: 3,
+        saldo: 4
+    },
+    {
+        mes: 4,
+        saldo: -5
+    },
+    {
+        mes: 5,
+        saldo: 6
+    },
+    {
+        mes: 6,
+        saldo: -7
+    },
+]
+
+for (let i = 0; i < ganancias.length; i++) {
+    //console.log('El mes' + ganancias[i].mes + 'tuvo un saldo de' + ganancias[i].saldo);    
+
+    if (ganancias[i].saldo >= 0) {
+        //console.log('El mes' + ganancias[i].mes + 'fue positivo');
+    }else{
+        //console.log('El mes' + ganancias[i].mes + 'fue negativo');
+    }
+
+};
+
+let personas = [
+    {
+        nombre: "Jon",
+        apellido: "Snow",
+        edad: 23,
+        ciudad: "Winterfell"
+    },
+    {
+        nombre: "Daenerys",
+        apellido: "Targaryen",
+        edad: 19
+    },
+    {
+        nombre: "Arya",
+        apellido: "Stark",
+        edad: 12,
+        ciudad: "Winterfell"
+    },
+    {
+        nombre: "Tyrion",
+        apellido: "Lannister",
+        edad: 32,
+        ciudad: "Casterly Rock"
+    }
+];
+
+let mensajes ='';
+
+for (let i = 0; i < personas.length; i++) {
+    
+    if (personas.edad <= 18 && personas.ciudad == null){
+        mensajes = `Hola ${personas.nombre} ${personas.apellido} criatura viajera!`
+    }else if (personas.edad >= 18 && personas.ciudad == null){
+        mensajes = `Hola ${personas.nombre} ${personas.apellido} eminencia viajera!`
+    }else if(personas.edad < 18 && personas.ciudad != undefined){
+        mensajes = `Hola mini ${personas.nombre} ${personas.apellido} de ${personas.ciudad}`
+    }else if(personas.edad > 18 && personas.ciudad != undefined){
+        mensajes = `Hola ${personas.nombre} ${personas.apellido} de ${personas.ciudad}`
+    }else if(personas.ciudad === 'Winterfell'){
+        mensajes = 'Winter is coming'
+    }
+}
+
+console.log(mensajes);
--- programacion2/Clase2/practica3/app.js
+++ programacion2/Clase2/practica3/app.js
@@ -1,29 +1,109 @@
-let edadMaxima = 100;
+/*CALCULADORA DE CONSUMO */
 
-function calcularConsumo(e,c,p) {
-    let diferencia = edadMaxima - e;
-    let diasConsumidos = diferencia*365;
-    let consumoTotal = diasConsumidos*c;
-        return 'Necesitaras' +consumoTotal + 'de' + p + 'para que dure toda tu vida.'
-}
+const calcularConsumo = (edad, consumo, producto) => {
 
-console.log(calcularConsumo(19,5, 'agua'))
+    const edadMaxima = 80;
+    let diferencia = edadMaxima - edad;
+    let dias = diferencia * 365
+
+    return "Necesitarás " + dias * consumo + " de " + producto +" para que dure el resto de tu vida";
+
+};
+
+/*
+console.log(calcularConsumo(21, 2, "medialunas"));
+*/
+
+/*EJ 2 CAMBIAR "EL ÚLTIMO ELEMENTO"*/
+
+let cambiarElUltimo = (array,dato) => {
+
+    array.pop();
+    array.push(dato);
+
+    return array ;
+};
+
+/*EJ 3 "TRABAJANDO CON OBJETOS" */
 
 let libro = {
-    nombre: 'The Shining',
-    genero: 'terror',
-    cantidadDePalabras: 500,
+    nombre:'Orgullo y prejuicio',
+    genero: 'Novela',
+    cantidadDePalabras: 555 ,
     paraAdultos: true,
 
-    function () {
-        return 'El libro se llama' + libro.nombre + 'y es del genero' + libro.genero
-    }
-}
+    descripcion: () => "El libro se llama " + libro.nombre + " y es de género " + libro.genero ,
 
-libro.puedeLeerlo = {
-    function (e) {
-        if (libro.paraAdultos === true && e<18){
+    puedeLeerlo: (edad) => {
+        if (edad<18 && libro.paraAdultos == true) {
             return false
         }
-    }
+        else {
+            return true
+        }  
+    
+    } 
+
 }
+
+//console.log(libro.descripcion());  
+//
+
+/* EJ 4: TRANSFORMANDO FUNCIONES EN ARROW FUNCTIONS */
+
+/* 1) Saludar
+function saludar(){
+  return “Hola”;
+};  */
+let saludar = () => "Hola"; 
+
+/* console.log(saludar()) */
+
+/*  2) Multiplicar
+function multiplicar(valor1, valor2){
+  return valor1*valor2;
+}; */
+
+let multiplicar = (valor1, valor2) => valor1*valor2; 
+
+/* console.log(multiplicar(2,3)) */
+
+
+/* 3) Anónima
+function (){
+  return “Soy una función sin nombre :( ”;
+}; 
+
+
+NO LA HICE */
+
+
+
+/* 4) Es par
+function esPar(numero){
+  return numero%2 === 0;
+};
+*/
+
+let esPar = (numero) => numero%2 === 0; 
+
+/* console.log(esPar(7)) */
+
+/*EJERCICIO 5*/
+
+let calcularAniosPerrunos = (e) => {
+    let equivalencia = e*7;
+    return 'Tu perro tiene' +equivalencia+ 'años perrunos'
+}
+
+//console.log(calcularAniosPerrunos(7));
+
+/*EJERCICIO 6*/
+
+let celsiusAFahrenheit = (t) =>{
+    let conversion = (t*9/5) +32;
+    return t + '°C son' + ''+ conversion + '°F'
+}
+console.log(celsiusAFahrenheit (0));
+
+
