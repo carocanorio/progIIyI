c96fb98ce46ae94e838d3f67993fe9abcfc5cfe1
carocanorio <ccanoriohellwing@udesa.edu.ar>
2022-03-13 12:28:17 -0300

    practica 4, me falta corregir

--- programacion2/Clase3/pruebaNPM/app.js
+++ programacion2/Clase3/pruebaNPM/app.js
@@ -13,13 +13,62 @@ function describirse (n) {
 //console.log(describirse(2));
 
 for (let i = 0; i < autos.lista.length; i++) {
-    console.log(describirse(i));
+    //console.log(describirse(i));
 };
 
 let porColor = c => {
-    if (c == ) {
-        
+    
+    let resultados = [];
+
+    for (let i = 0; i < autos.lista.length; i++) {
+
+        if (autos.lista[i].color === c) {
+            resultados.push(autos.lista[i]); 
+        }else{
+            resultados.push('No contamos con autos del color' +c);
+        }
+
     }
+
+    return resultados
 }
 
+//console.log(porColor('negro'));
 
+let porMarca = m => {
+    
+    let arrayDeMarcas = [];
+
+    for (let i = 0; i < autos.lista.length; i++) {
+
+        if (autos.lista[i].marca === m) {
+            arrayDeMarcas.push(autos.lista[i]); 
+        }else{
+            arrayDeMarcas.push('No contamos con autos de la marca' + ' ' + m);
+        }
+
+    }
+
+    return arrayDeMarcas
+}
+
+//console.log(porMarca('Volkswagen'));
+
+let porAnio = a=> {
+    
+    let arrayDeAnios = [];
+
+    for (let i = 0; i < autos.lista.length; i++) {
+
+        if (autos.lista[i].anio === a) {
+            arrayDeAnios.push(autos.lista[i]); 
+        }else{
+            arrayDeAnios.push('No contamos con autos del anio' + ' ' + a);
+        }
+
+    }
+
+    return arrayDeAnios
+}
+
+console.log(porAnio(2021));
